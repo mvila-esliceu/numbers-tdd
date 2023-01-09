ad0dc579ba14516cfe988fe7d21ad0416d621a36
marc.vila <marc.vila@axisdata.net>
2023-01-09 11:42:12 +0100

    Test 2 - Dos

--- src/main/java/NumbersCat.java
+++ src/main/java/NumbersCat.java
@@ -2,6 +2,8 @@ public class NumbersCat {
     public static String say(long number) {
         if(number == 1) {
             return "Un";
+        } else if (number == 2) {
+            return "Dos";
         }
         return "Zero";
     }
